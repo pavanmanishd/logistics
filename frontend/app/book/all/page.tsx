55c7c0fc6efb9f775801a7bb9a2f066f785fd0d6
pavanmanishd <pavanmanishd@gmail.com>
2024-10-15 11:00:37 +0530

    protect /book/all route

--- frontend/app/book/all/page.tsx
+++ frontend/app/book/all/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import ProtectedRoute from "@/validation/ProtectedRoute";
 
 const bookingsAPIURL = "http://localhost:8081";
 
@@ -43,25 +44,27 @@ export default function AllBookings() {
     }, [userid]);
 
     return (
-        <div>
-            <h1>All Bookings</h1>
-            {bookings.length > 0 ? (
-                <ul>
-                    {bookings.map((booking) => (
-                        <li key={booking.id}>
-                            <p>Booking ID: {booking.id}</p>
-                            <p>User ID: {booking.user_id}</p>
-                            <p>Driver ID: {booking.driver_id}</p>
-                            <p>Status: {booking.status}</p>
-                            <p>Source: {booking.source.coordinates.join(", ")}</p>
-                            <p>Destination: {booking.destination.coordinates.join(", ")}</p>
-                            <p>Fare: ${booking.fare.toFixed(2)}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No bookings found.</p>
-            )}
-        </div>
+        <ProtectedRoute element={
+            <div>
+                <h1>All Bookings</h1>
+                {bookings.length > 0 ? (
+                    <ul>
+                        {bookings.map((booking) => (
+                            <li key={booking.id}>
+                                <p>Booking ID: {booking.id}</p>
+                                <p>User ID: {booking.user_id}</p>
+                                <p>Driver ID: {booking.driver_id}</p>
+                                <p>Status: {booking.status}</p>
+                                <p>Source: {booking.source.coordinates.join(", ")}</p>
+                                <p>Destination: {booking.destination.coordinates.join(", ")}</p>
+                                <p>Fare: ${booking.fare.toFixed(2)}</p>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No bookings found.</p>
+                )}
+            </div>
+        } />
     );
 }
